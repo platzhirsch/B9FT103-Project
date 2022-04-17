47b98ea230475ac1080d970c7fe7216525b2c79e
Felix Flassak <felixflassak@gmail.com>
2022-04-17 12:49:50 +0100

    refactor create blockchainService

--- python/Script.py
+++ python/Script.py
@@ -1,52 +1,9 @@
-import os
 from sys import argv
-import binascii
-from dotenv import load_dotenv
-from pathlib import Path
-from web3 import Web3
 import datetime
-import json 
 import hashlib
 import typer
-
-
-#define path to load data from .env file 
-dotenv_path = Path('./.env')
-load_dotenv(dotenv_path=dotenv_path)
-
-#define web3 RPC provider
-rpc_provider = os.getenv('RPC_PROVIDER')
-w3 =  Web3(Web3.HTTPProvider(rpc_provider))
-
-#define own adress
-wallet_address = '0x090f395Bd24E8Ba7e0A2730ADB387d5Be9428Af5'
-wallet_address = Web3.toChecksumAddress(wallet_address)
-private_key = os.getenv('PRIVATE_KEY')
-
-#define contract
-contract_address = '0x715514960eac1ef8b06b2d3779dcfaa29c6e61ba'
-contract_address = Web3.toChecksumAddress(contract_address)
-ABI = json.loads('[{"inputs":[{"internalType":"string","name":"_greeting","type":"string"}],"name":"setGreeting","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"greet","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"greeting","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]')
-contract = w3.eth.contract(contract_address, abi=ABI)
-
-
-
-#change file hash 
-def writeHashToBC(filehash):
-    nonce = w3.eth.getTransactionCount(wallet_address)
-    transaction = contract.functions.setGreeting(filehash).buildTransaction({
-    'chainId': 4,
-    'gas': 1400000,
-    'gasPrice': w3.toWei('160', 'gwei'),
-    'nonce': nonce,
-    'from': wallet_address
-    }) 
-    signed_txn = w3.eth.account.signTransaction(transaction, private_key=private_key)
-    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-    #tx_hash is in ASCII binary 
-    return binascii.hexlify(tx_hash)
-
+from blockchainService import writeHashToBC
+from blockchainService import getFileHash
 
 
 #read file return hash 
@@ -88,7 +45,6 @@ def checkFilelenght():
      lines = f.readlines()
      for line in lines:
          if line.find('---- FILE ENDS HERE ----') != -1:
-            print (lines.index(line))
             return lines.index(line)
      else: 
         num_lines = sum(1 for line in open('./file.txt'))
@@ -96,13 +52,6 @@ def checkFilelenght():
                 f.write("---- FILE ENDS HERE ----")
         return num_lines
         
-          
-
-
-def getFileHash():
-    ethFilehash = contract.functions.greet().call()
-    return ethFilehash
-
 
 def checkFileIntegrity():
     if getFileHash() == hashFile():
